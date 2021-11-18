382643770dd3f63ec50482c53af77c4e31f591b6
wladyband <tecnico100@gmail.com>
2021-11-18 08:55:31 -0300

    configurando data e hora na tela de captura

--- AutomacaoTest/core/BasePage.cs
+++ AutomacaoTest/core/BasePage.cs
@@ -1,19 +1,21 @@
 ﻿using OpenQA.Selenium;
 using System;
-using System.IO;
+using System.Globalization;
 using static AutomacaoTest.core.DriverFactory;
 
 namespace AutomacaoTest.core
 {
     public class BasePage
     {
+       
         private static String URL_SISTEMA = "http://automationpractice.com/index.php";
         private static String CAMINHO_IMAGEM = "C://temp//Image.png";
 
         public void capturaTela() {
-            Screenshot ss = ((ITakesScreenshot)getDriver()).GetScreenshot();
-            ss.SaveAsFile(CAMINHO_IMAGEM, ScreenshotImageFormat.Png);
-
+            Screenshot imagem = ((ITakesScreenshot)getDriver()).GetScreenshot();
+            string dataHora = DateTime.Now.ToString("yyyy-MM-dd-HH_mm_ss", CultureInfo.GetCultureInfo("pt-BR"));
+            String telaCapturada = CAMINHO_IMAGEM + "telaCapturada" + "_" + dataHora + ".png";
+            imagem.SaveAsFile(telaCapturada, ScreenshotImageFormat.Png);
         }
 
         public void acessarSistema()
